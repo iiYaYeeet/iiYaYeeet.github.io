b63103f33e25755c06f01cf57d05ab742c8423e7
MSI\CJRow <cjrowland05@gmail.com>
2024-12-11 16:54:10 -0500

    commented

--- sandbox.js
+++ sandbox.js
@@ -4,11 +4,13 @@
 	const newElement = document.createElement("div");	
 	const view =[];
 	const downs =[];
+	//pull from api
 	function pullapi() {
 	fetch("https://cors-anywhere.herokuapp.com/https://itch.io/api/1/98EDYuzPNLo22ofUPRd4IH4vFYlVQLlesraGAwYK/my-games",)
 	.then(res => {
 	return res.json();
 	})
+	//filter data
 		.then(data=>{
 		data.games.forEach(object => {
         var views = object.views_count;
@@ -16,6 +18,7 @@
 		view.push(views);
 		downs.push(dowloads);
 	})
+	//concentrate
 		const totalviews = view.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 		const totaldownloads = downs.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 		console.log(totalviews);
@@ -23,6 +26,7 @@
 		replacedata(totalviews,totaldownloads);
 	})
 	}
+	//push it out
 	function replacedata(v,d) {
 		const values = document.getElementsByClassName("displays")[0];
 		values.getElementsByClassName("views")[0].innerHTML = v;
