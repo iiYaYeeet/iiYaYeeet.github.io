db1e69ef5b32b9e97fff0bf68a8a140b936644f9
MSI\CJRow <cjrowland05@gmail.com>
2024-12-11 16:22:59 -0500

    oh my god it works

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,7 @@
 	var totalviews;
 	var totaldownloads;
+	const oldElement = document.getElementById("views");
+	const newElement = document.createElement("div");	
 	const view =[];
 	const downs =[];
 	fetch("https://cors-anywhere.herokuapp.com/https://itch.io/api/1/98EDYuzPNLo22ofUPRd4IH4vFYlVQLlesraGAwYK/my-games",)
@@ -7,20 +9,21 @@
 	return res.json();
 	})
 		.then(data=>{
-		console.log(data);
 		data.games.forEach(object => {
         var views = object.views_count;
         var dowloads = object.downloads_count;
 		view.push(views);
 		downs.push(dowloads);
 	})
-		console.log(view);
 		const totalviews = view.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-		console.log(downs);
 		const totaldownloads = downs.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 		console.log(totalviews);
 		console.log(totaldownloads);
-		document.writeln("<p>Total View Count <strong>" + totalviews + "</strong>.</p>");
-		document.writeln("<p>Total Download Count <strong>" + totaldownloads + "</strong>.</p>");
+		replacedata(totalviews,totaldownloads);
 	})
-.catch(error=>console.log(error));
+
+	function replacedata(v,d) {
+		const values = document.getElementsByClassName("displays")[0];
+		values.getElementsByClassName("views")[0].innerHTML = v;
+		values.getElementsByClassName("downloads")[0].innerHTML = d;
+	}
